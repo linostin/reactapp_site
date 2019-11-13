1b5f417e1f29ce17de523824c4c87046f614010b
linostin <lifesins@gmail.com>
2019-11-14 02:22:16 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/Header.css"
+
+function Header() {
+    return(
+        <header className="navbar">This is the header</header>
+    )
+}
+
+//const App = () => <h1>Hello world!</h1>
+//стрелочная функция
+
+export default Header;
