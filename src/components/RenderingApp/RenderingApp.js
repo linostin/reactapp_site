48d2f5469ac0a186738ef813755bdecee96a9e4d
linostin <lifesins@gmail.com>
2019-11-21 02:08:36 +0300

    21 lesson

---
+++ src/components/RenderingApp/RenderingApp.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class RenderingApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      unreadMessages: [
+        "Call your mom!",
+        "New spam email available. All links are definitely safe to click."
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.unreadMessages.length > 0 && (
+          <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
+        )}
+      </div>
+    );
+  }
+}
+
+export default RenderingApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import LifeCycleApp from "./components/ComponentLifecycle/LifeCycleApp";
+//import LifeCycleApp from "./components/ComponentLifecycle/LifeCycleApp";
+import RenderingApp from "./components/RenderingApp/RenderingApp";
 
-ReactDOM.render(<LifeCycleApp />, document.getElementById("root"));
+ReactDOM.render(<RenderingApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
