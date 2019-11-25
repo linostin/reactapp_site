627b2ac5130a177b78566f3b086eaaa57c80cfec
linostin <lifesins@gmail.com>
2019-11-25 22:05:11 +0300

    21 lesson login_logout

--- src/components/RenderingApp/LoginLogoutRendering.js
+++ src/components/RenderingApp/LoginLogoutRendering.js
@@ -1,19 +1,34 @@
 import React from "react"
 
-class App extends React.Component {
+class LoginLogoutRendering extends React.Component {
     constructor() {
         super()
         this.state = {
             isLoggedIn: false
         }
+        this.handleClick = this.handleClick.bind(this)
     }
+
+    handleClick() {
+        this.setState(pervState => {
+            return {
+               isLoggedIn: !pervState.isLoggedIn
+            };
+        });
+    }
+
+
+
     render() {    
+        let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+        let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out"
         return (
             <div>
-                Code goes here
+                <h1>{displayText}</h1>
+                <button onClick={this.handleClick}>{buttonText}</button>
             </div>
         )
     }
 }
 
-export default App
+export default LoginLogoutRendering
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 //import LifeCycleApp from "./components/ComponentLifecycle/LifeCycleApp";
-import RenderingApp from "./components/RenderingApp/RenderingApp";
+//import RenderingApp from "./components/RenderingApp/RenderingApp";
+import LoginLogoutRendering from "./components/RenderingApp/LoginLogoutRendering"
 
-ReactDOM.render(<RenderingApp />, document.getElementById("root"));
+ReactDOM.render(<LoginLogoutRendering />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
