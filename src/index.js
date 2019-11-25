78cf943404e45015b7c531eafb7db5556f526f27
linostin <lifesins@gmail.com>
2019-11-25 22:30:37 +0300

    api fetch 22 lesson

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import DataFrom from "./components/DataFromAPI/dataFromApi";
 //import LifeCycleApp from "./components/ComponentLifecycle/LifeCycleApp";
 //import RenderingApp from "./components/RenderingApp/RenderingApp";
-import LoginLogoutRendering from "./components/RenderingApp/LoginLogoutRendering"
+//import LoginLogoutRendering from "./components/RenderingApp/LoginLogoutRendering"
 
-ReactDOM.render(<LoginLogoutRendering />, document.getElementById("root"));
+ReactDOM.render(<DataFrom />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
