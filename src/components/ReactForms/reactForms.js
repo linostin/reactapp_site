6b64fd0a6b27b6d6d7ea8ee42fb60f78145c750d
linostin <lifesins@gmail.com>
2019-11-25 23:48:09 +0300

    23 lesson react form

---
+++ src/components/ReactForms/reactForms.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+class ReactForms extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                Code here
+            </div>
+        )
+    }
+}
+
+
+export default ReactForms
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import DataFrom from "./components/DataFromAPI/dataFromApi";
+//import DataFrom from "./components/DataFromAPI/dataFromApi";
+import ReactForms from "./components/ReactForms/reactForms";
 //import LifeCycleApp from "./components/ComponentLifecycle/LifeCycleApp";
 //import RenderingApp from "./components/RenderingApp/RenderingApp";
 //import LoginLogoutRendering from "./components/RenderingApp/LoginLogoutRendering"
 
-ReactDOM.render(<DataFrom />, document.getElementById("root"));
+ReactDOM.render(<ReactForms />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
