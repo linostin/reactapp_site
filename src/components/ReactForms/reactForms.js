745c3d9f2bb77de1308ac472965c6bd640761959
linostin <lifesins@gmail.com>
2019-11-27 02:17:58 +0300

    22 lesson part2 react forms

--- src/components/ReactForms/reactForms.js
+++ src/components/ReactForms/reactForms.js
@@ -1,20 +1,92 @@
-import React from "react"
+import React from "react";
 
 class ReactForms extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      isFriendly: true
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    constructor() {
-        super()
-        this.state = {}
-    }
+  handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({ [name]: value });
+  }
 
-    render() {
-        return (
-            <div>
-                Code here
-            </div>
-        )
-    }
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          value={this.state.firstName}
+          name="firstName"
+          placeholder="First Name"
+          onChange={this.handleChange}
+        />
+
+        <br />
+
+        <input
+          type="text"
+          value={this.state.lastName}
+          name="lastName"
+          placeholder="Last Name"
+          onChange={this.handleChange}
+        />
+
+        <br />
+
+        <textarea value={"Some default value"} onChange={this.handleChange} />
+
+        <br />
+
+        <label>
+          <input
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />{" "}
+          is Friendly?
+        </label>
+
+        <br/>
+
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />{" "}
+          Male
+        </label>
+
+        <br />
+
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />{" "}
+          Female
+        </label>
+
+        <h1>
+          {this.state.firstName} {this.state.lastName}
+        </h1>
+      </form>
+    );
+  }
 }
 
-
-export default ReactForms
+export default ReactForms;
