4a96286a81cee58093eb789e6f5d88e5f5f9b6f6
linostin <lifesins@gmail.com>
2020-01-05 20:13:00 +0300

    lesson 24

--- src/components/TestSPA/components/Dialogs/Dialogs.jsx
+++ src/components/TestSPA/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import styles from "./Dialogs.module.css"
 import mainstyles from "./../Maincontent/Maincontent.module.css";
+import { NavLink } from 'react-router-dom';
+
+
+const DialogItem = (props) => {
+
+    let path = "/Dialogs/" + props.id
+
+    return (
+        <div>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const MessagesItem = (props) => {
+    return (
+        <div>
+            {props.message}
+        </div>
+    )
+}
 
 const Dialogs = () => {
     return (
@@ -8,9 +29,15 @@ const Dialogs = () => {
             <div className={styles.dialogs}>
             <div className={styles.users}>
                 users
+                <DialogItem name="user1" id="1" />
+                <DialogItem name="user2" id="2" />
+                <DialogItem name="user3" id="3" />
             </div>
             <div className={styles.messages}>
                 messages
+                <MessagesItem message="Hello!" />
+                <MessagesItem message="How are you?" />
+                <MessagesItem message="Where you from?" />
             </div>
             </div>
         </div>
